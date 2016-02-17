3d7c542056113b0cabd065b56eef55ad851ecb52
angelxut <angelx@utexas.edu>
2016-02-17 11:01:05 -0600

    add loading screen

--- MeetApp/MeetApp/ViewController.swift
+++ MeetApp/MeetApp/ViewController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HolderViewDelegate {
+    
+    var holderView = HolderView(frame:CGRectZero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        addHolderView()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func addHolderView() {
+        let boxSize: CGFloat = 100.0
+        holderView.frame = CGRect(x: view.bounds.width / 2 - boxSize / 2,
+            y: view.bounds.height / 2 - boxSize / 2,
+            width: boxSize,
+            height: boxSize)
+        holderView.parentFrame = view.frame
+        holderView.delegate = self
+        view.addSubview(holderView)
+        holderView.addOval()
+    }
+    
+    func backgroundLabel() {
+    }
+    
+    func addButton() {
+        let button = UIButton()
+        button.frame = CGRectMake(0.0, 0.0, view.bounds.width, view.bounds.height)
+        button.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+        view.addSubview(button)
+    }
+    
+    func buttonPressed(sender: UIButton!) {
+        view.backgroundColor = Colors.white
+        view.subviews.map({ $0.removeFromSuperview() })
+        holderView = HolderView(frame: CGRectZero)
+        addHolderView()
     }
 
 
