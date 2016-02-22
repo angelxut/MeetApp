1d0c4d49a6b148d85574153303ad9264262a151e
nobody6501 <martin@Martins-Mac.local>
2016-02-22 06:42:28 -0800

    new map commit, no pods

--- MeetApp/MeetApp/AppDelegate.swift
+++ MeetApp/MeetApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    let googleMapKey = "AIzaSyCNIhicJjgnmcuRy3hDP8t0jsAfA9BnY6M"
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey(googleMapKey)
         return true
     }
 
